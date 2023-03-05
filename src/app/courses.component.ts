76b5e8f33fde65be86102d30a4b71a4b1dec4ceb
Geetika Chauhan <geetika.chauhan.17@gmail.com>
2023-03-05 22:10:11 +0530

    angular directives example

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,13 +1,21 @@
-import {Component} from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'courses',
-  template: '<h2>{{ getTitle() }}</h2>'
+  template: `
+    <h2>{{ getTitle() }}</h2>
+    <ul>
+      <li *ngFor="let course of courses">
+        {{ course }}
+      </li>
+    </ul>
+  `,
 })
 export class CoursesComponent {
-  title = "List of Courses"
+  title = 'List of Courses';
+  courses = ['course 1', 'course 2', 'course 3'];
 
   getTitle() {
-    return this.title
+    return this.title;
   }
 }
